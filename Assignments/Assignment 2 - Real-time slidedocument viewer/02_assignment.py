7025e80191203cd8a51b8f753128823b19be27c3
Felix Meyer-Veit <felix.meyer-veit@web.de>
2020-11-24 22:25:19 +0000

    1. Capture an image from a video camera

--- Assignments/Assignment 2 - Real-time slidedocument viewer/02_assignment.py
+++ Assignments/Assignment 2 - Real-time slidedocument viewer/02_assignment.py
@@ -70,10 +70,12 @@ while (True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    if cv2.waitKey(1) == 32: # SPACE pressed
+    # press 'c' to capture image
+    if cv2.waitKey(1) & 0xFF == ord('c'):
         img_name = "opencv_frame_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
+        print("{}".format(img_name))
         img_counter += 1
 
 
