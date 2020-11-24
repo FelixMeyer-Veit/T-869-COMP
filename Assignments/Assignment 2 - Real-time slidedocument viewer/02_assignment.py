400239e34d6855374a4c2cb7226ffd4d24be1d71
Felix Meyer-Veit <felix.meyer-veit@web.de>
2020-11-24 22:13:19 +0000

    capture image

--- Assignments/Assignment 2 - Real-time slidedocument viewer/02_assignment.py
+++ Assignments/Assignment 2 - Real-time slidedocument viewer/02_assignment.py
@@ -1,18 +1,45 @@
 ################################################ IMPORT ################################################################
-# import the opencv library
 import cv2
 import time
-import numpy
 
+################################################ FUNCTIONS #############################################################
+# get average of a list
+def Average(lst):
+    return sum(lst) / len(lst)
+
+def get_fps(frame, prev_frame_time):
+    # time when we finish processing for this frame
+    new_frame_time = time.time()
+
+    # Calculating the fps
+
+    # fps will be number of frame processed in given time frame
+    # since their will be most of time error of 0.001 second
+    # we will be subtracting it to get more accurate result
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+
+    # converting the fps into integer
+    fps = int(fps)
+
+    # add current fps at the end of array
+    lst.append(fps)
+
+    # get the average of processing time
+    average = int(Average(lst))
+
+    # converting the fps to string so that we can display it on frame
+    # by using putText function
+    fps_avg = str(average)
+
+    # puting the FPS count on the frame
+    cv2.putText(frame, fps_avg, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+
+    return prev_frame_time
 
 ################################################ DECLARATIONS ##########################################################
 # define a video capture object (webcam)
-#vid = cv2.VideoCapture(0)
-
-# with mobile phone
-URL = 'http://192.168.10.102:6677/videoView'
-vid = cv2.VideoCapture(URL)
-
+vid = cv2.VideoCapture(0)
 
 # used to record the time when we processed last frame
 prev_frame_time = 0
@@ -23,9 +50,40 @@ new_frame_time = 0
 # create array for getting average
 lst = []
 
-################################################ FUNCTIONS #############################################################
-# get average of a list
-def Average(lst):
-    return sum(lst) / len(lst)
+img_counter = 0
 
 ################################################ MAIN ##################################################################
+while (True):
+
+    # Capture the video frame by frame
+    ret, frame = vid.read()
+
+    # font which we will be using to display FPS
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # get fps and store a new prev_frame_time
+    prev_frame_time = get_fps(frame, prev_frame_time)
+
+    # Display the resulting frame
+    cv2.imshow('Webcam', frame)
+
+    # press 'q' to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    if cv2.waitKey(1) == 32: # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+
+# Driver Code
+average = Average(lst)
+
+# Printing average of the list
+print("Average processing time =", round(average, 2))
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
